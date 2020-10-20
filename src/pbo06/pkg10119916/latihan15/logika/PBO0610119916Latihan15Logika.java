36525036c5d46579eb17efd012b64a2f0a2a0ca8
DianArpian <>
2020-10-20 20:51:54 +0700

    Logika

---
+++ src/pbo06/pkg10119916/latihan15/logika/PBO0610119916Latihan15Logika.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo06.pkg10119916.latihan15.logika;
+
+/**
+ *
+ * @author 
+ * NAMA     : DIAN ARPIAN
+ * KELAS    : IF10K
+ * NIM      : 10119916 
+ * Deskripsi Program : : Logika
+ */
+public class PBO0610119916Latihan15Logika {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       
+        boolean a = true;
+        boolean b = false;
+        System.out.println("a && b = " + (a && b));
+        System.out.println("a || b = " + (a || b));
+        System.out.println("!a = " + !a);
+        System.out.println("!(a && b) = " + !(a && b));
+    }
+    
+}
